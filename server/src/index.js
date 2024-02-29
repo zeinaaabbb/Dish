3179aea3364b3c210e718bc7ed8d8e23528d97fa
zeinab <swarsama@hotmail.co.uk>
2024-02-29 20:11:23 +0000

    updates script in packages

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,11 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express()
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
