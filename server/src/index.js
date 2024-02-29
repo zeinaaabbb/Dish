eabc93b03df345b3d3aa8b65a473016cf2b78869
zeinab <swarsama@hotmail.co.uk>
2024-02-29 22:26:10 +0000

    updates to file

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express()
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.)
 
 app.listen(3001, () => console.log("SERVER STARTED!"));
