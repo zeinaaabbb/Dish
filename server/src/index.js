1ffaa44ac0ff450dd48956b058c545b425aae770
zeinab <swarsama@hotmail.co.uk>
2024-03-02 16:17:08 +0000

    database connected

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,15 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const app = express()
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.)
+// console.log(process.env.mongoDB);
+mongoose.connect(process.env.mongoDB);
 
 app.listen(3001, () => console.log("SERVER STARTED!"));
