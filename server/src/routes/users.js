f418b3934642a968028b513ba9584706c938dd55
zeinab <swarsama@hotmail.co.uk>
2024-03-03 09:54:37 +0000

    added pseudocode

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,20 +5,22 @@ import { UserModel } from "../models/Users.js"
 
 const router = express.Router();
 
-//post request /register
+//post request - registration endpoint
 router.post("/register", async (req, res) => {
+  //Request Body Extraction:
   const { username, password } = req.body;
   // console.log("Received registration request:", { username, password });
   const user = await UserModel.findOne({ username });
+  //Check if User Exists
   if (user) {
     return res.json({message: "User already exists!"});
   }
-
+  //Hash Password:
   const hashedPassword = await bcrypt.hash(password, 10);
-
+  //Create New User Instance:
   const newUser = new UserModel({username, password: hashedPassword});
+  //Save User to Database:
   await newUser.save();
-
   res.json({message: "User Registered Successfully"});
 });
 
