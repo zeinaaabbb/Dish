69103769bd01c9c728a9b331286d4d08cb02b949
zeinab <swarsama@hotmail.co.uk>
2024-03-05 13:45:07 +0000

    created register function

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,7 +1,46 @@
+import React, {useState} from 'react'
+
 function Auth() {
   return (
-    <div>Auth</div>
+    <div className="auth">
+      <Login/>
+      <Register/>
+    </div>
   );
 }
+
+const Login = () => {
+  return <div></div>;
+};
+
+const Register = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div className="auth-container">
+    <form>
+      <h2>Register</h2>
+      <div className="form-group">
+        <label htmlFor="username"> Username: </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password"> Password: </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)} />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>;
+};
+
 
 export default Auth
