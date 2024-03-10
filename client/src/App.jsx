fad878822ff26c37d246bb1c0cddfae70e06c46f
zeinab <swarsama@hotmail.co.uk>
2024-03-10 22:52:03 +0000

    created css files and working on navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import CreateRecipe from './pages/CreateRecipe';
 import SavedRecipe from './pages/SavedRecipe';
 import Navbar from './components/Navbar';
 
+
 function App() {
   return (
     <div className="App">
