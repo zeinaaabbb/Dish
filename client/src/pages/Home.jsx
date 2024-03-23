cc0625d41ece17e32c1318af4e44aabee7948fa9
zeinab <swarsama@hotmail.co.uk>
2024-03-23 05:43:41 +0000

    home section fetching API is coming together

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Home() {
+
+const [ recipes, setRecipes ] = useState([]);
+
+useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const result = await axios.get("http://localhost:3001/recipes");
+        setRecipes(result.data);
+        // console.log(result.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchRecipe();
+
+});
+
+
   return (
-    <div>Home</div>
+    <div>
+        <h1>Recipes</h1>
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe._id}>
+            <div>
+              <h1>{recipe.name}</h1>
+            </div>
+            <div>
+              <p> {recipe.descriptions}</p>
+            </div>
+            <div>
+            <p> {recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name}/>
+            <p>Cooking Time: {recipe.cookingTime} (mins)</p>
+            </li>
+          ))};
+          </ul>
+      </div>
   );
 }
 
