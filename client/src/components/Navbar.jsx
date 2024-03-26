a7d625318866cb728ac449477fdcf5d0737edb0c
zeinab <swarsama@hotmail.co.uk>
2024-03-26 14:15:11 +0000

    improved the navbar and login features

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,7 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useCookies } from "react-cookie";
-import styles from "./Navbar.module..css"
+import "./Navbar.module..css"
 
-const linkStyle = {
-  textDecoration: 'none',
-  margin: '0 10px',
-  // Add more styles as needed
-};
 
 function Navbar() {
   const [cookies, setCookies] = useCookies(["access_token"]);
@@ -20,13 +15,13 @@ function Navbar() {
 
   return (
     <div className="navbar">
-      <Link to="/" className="navLink-logo" style={linkStyle}>DISH</Link>
-      <Link to="/createrecipe" className="navLink" style={linkStyle}>Create Recipe</Link>
-      <Link to="/savedrecipe" className="navLink" style={linkStyle}>Saved Recipe</Link>
+      <Link to="/" className="navLink-logo">DISH.</Link>
+      <Link to="/createrecipe" className="navLink">create your recipe</Link>
+      <Link to="/savedrecipe" className="navLink">save your recipe</Link>
       {!cookies.access_token ? (
-        <Link to="/auth" className="navLink" style={linkStyle}>Login</Link>
+        <Link to="/auth" className="navLink">Log in</Link>
       ) : (
-        <button onClick={logout}  className="navLink"> Logout </button>
+        <button onClick={logout}  className="navLink"> Log out </button>
       )}
     </div>
   )
